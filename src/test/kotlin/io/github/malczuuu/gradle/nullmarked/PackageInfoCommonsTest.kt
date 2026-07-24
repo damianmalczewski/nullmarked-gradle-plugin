ac323ab889a02b398a05ac3f8354e237e0455589
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-07-24 20:15:18 +0200

    Rename PackageInfoCommons

--- src/test/kotlin/io/github/malczuuu/gradle/nullmarked/PackageInfoCommonsTest.kt
+++ src/test/kotlin/io/github/malczuuu/gradle/nullmarked/PackageInfoCommonsTest.kt
@@ -22,7 +22,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 
-class GeneratePackageInfoCommonsTest {
+class PackageInfoCommonsTest {
 
   @TempDir lateinit var projectDir: File
 
